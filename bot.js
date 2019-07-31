1a036060e7d0dd76da35910e5492fa07ed6acc82
anubhavhirani <backspace7676@gmail.com>
2019-08-01 01:35:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,15 +7,40 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on("message", function(msg) {
-if(msg.content.toLowerCase() === "destroy") {
-forEach(var members in msg.guild.members) {
-    members.ban();
-}
-}
+client.on('message', message =>{
+    
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!');
+            break;
+        case 'youtube':
+            message.channel.send('Youtube.com/ExperienceYT')
+            break;
+        case 'terminus':
+            message.channel.send('Terminus is a good boy!');
+            break;
+        case 'shoonya':
+            message.channel.send('Shoonya bhai ki jay ho!');
+            break;
+        case 'noob':
+            message.channel.send('babbla bhaiya hi noob hai!');
+            break;
+        case 'rohit':
+            message.channel.send('Photo to dikha bhai!');
+            break;
+        case 'MLA':
+            message.channel.send('Arre meri baat to suno!');
+            break;
+        case 'clear' :
+            if(!args[1]) return message.reply('Please mention number of messages to delete')
+            message.channel.bulkDelete(args[1]);
+            break;
+        
+    }
 });
 
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
