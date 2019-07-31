1e1a3c1d2db0b73e2f29a636aba3f396152097f8
anubhavhirani <backspace7676@gmail.com>
2019-07-31 22:31:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,25 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', message =>{
+    
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!');
+            break;
+        case 'youtube':
+            message.channel.send('Youtube.com/ExperienceYT')
+            break;
+        case 'clear' :
+            if(!args[1]) return message.reply('Please mention number of messages to delete')
+            message.channel.bulkDelete(args[1]);
+            break;
+        
+    }
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
