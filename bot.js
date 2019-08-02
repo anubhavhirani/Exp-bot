ad1b5f40a8586bd690a5e65e88299bf76305ccb1
anubhavhirani <backspace7676@gmail.com>
2019-08-03 01:03:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ client.on('message', message => {
         case 'nav':
             message.channel.send('Oye saale kya bola mujhe, main sardar hu isme meri galti nahi hai!');
             break;
-        case '.DeHu':
+        case 'DeHu':
             message.channel.send('Main hi CS:GO hacker hu aimbot chalata hu AWP pe!');
             break;
     }
