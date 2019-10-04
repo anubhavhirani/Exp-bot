833bff2efb8398e8253175f4c130fd9597663c84
anubhavhirani <backspace7676@gmail.com>
2019-10-05 04:04:35 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,8 @@ let jeetohMoveEnabled = false
 let mbMoveEnabled = false
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('I am ready!')
+    client.user.setActivity("ExperienceYT on Instagram")
 });
 
 client.on('voiceStateUpdate', (oldUser, newUser) => {
