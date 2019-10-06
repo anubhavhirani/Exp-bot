e89c205547e7fcce6b58cf7a77b206ed56aba977
anubhavhirani <backspace7676@gmail.com>
2019-10-07 03:19:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ let jeetohMoveEnabled = false
 let mbMoveEnabled = false
 
   client.on('ready', () => {
-  console.log('I am ready!');
-    client.user.setPresence("my code", { type: "Watching"})
+   client.user.setActivity("my code", { type: "Watching"})
+    console.log('I am ready!');
 });
 
 client.on('voiceStateUpdate',(oldMember,newMember)=>{
