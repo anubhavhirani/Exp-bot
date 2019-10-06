0e1a5158dd765eaeb67fc3e188277d87d03208a4
anubhavhirani <backspace7676@gmail.com>
2019-10-06 08:00:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,6 @@ client.on('message', message => {
                  "name": " `Default prefix is set to '.'` ",
                     "description": " `Here are all the commands for your server:` ",
                     "color": 16738560,
-                    "timestamp": "2019-10-06T01:21:31.044Z",
                     "footer": {
                       "icon_url": "https://cdn.discordapp.com/attachments/629032504644599809/630217087666618408/icon-2382008_960_720.png",
                       "text": "VC Mover"
