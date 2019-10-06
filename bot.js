a551ffc618f4cefa8c0f15aaa32151df0594dc8c
anubhavhirani <backspace7676@gmail.com>
2019-10-06 06:54:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,6 +134,55 @@ client.on('message', message => {
             if (!args[1]) return message.reply('Please mention number of messages to delete')
             message.channel.bulkDelete(args[1]);
             break;
+        case 'help':
+            const embed = {
+                "title": "title ~~(did you know you can have markdown here too?)~~",
+                "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+                "url": "https://discordapp.com",
+                "color": 1560522,
+                "timestamp": "2019-10-06T01:21:31.044Z",
+                "footer": {
+                  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                  "text": "footer text"
+                },
+                "thumbnail": {
+                  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                },
+                "image": {
+                  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                },
+                "author": {
+                  "name": "author name",
+                  "url": "https://discordapp.com",
+                  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                },
+                "fields": [
+                  {
+                    "name": "🤔",
+                    "value": "some of these properties have certain limits..."
+                  },
+                  {
+                    "name": "😱",
+                    "value": "try exceeding some of them!"
+                  },
+                  {
+                    "name": "🙄",
+                    "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+                  },
+                  {
+                    "name": "<:thonkang:219069250692841473>",
+                    "value": "these last two",
+                    "inline": true
+                  },
+                  {
+                    "name": "<:thonkang:219069250692841473>",
+                    "value": "are inline fields",
+                    "inline": true
+                  }
+                ]
+              };
+              channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
+            break;    
     }
 });
 
