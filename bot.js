d76e11f57b28e533cd972a38248554cadeab679a
anubhavhirani <backspace7676@gmail.com>
2019-10-06 07:02:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,52 +136,52 @@ client.on('message', message => {
             break;
         case 'help':
             const embed = {
-                "title": "title ~~(did you know you can have markdown here too?)~~",
-                "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
-                "url": "https://discordapp.com",
-                "color": 1560522,
-                "timestamp": "2019-10-06T01:21:31.044Z",
-                "footer": {
-                  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-                  "text": "footer text"
-                },
-                "thumbnail": {
-                  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                },
-                "image": {
-                  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                },
-                "author": {
-                  "name": "author name",
-                  "url": "https://discordapp.com",
-                  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                },
-                "fields": [
-                  {
-                    "name": "🤔",
-                    "value": "some of these properties have certain limits..."
-                  },
-                  {
-                    "name": "😱",
-                    "value": "try exceeding some of them!"
-                  },
-                  {
-                    "name": "🙄",
-                    "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
-                  },
-                  {
-                    "name": "<:thonkang:219069250692841473>",
-                    "value": "these last two",
-                    "inline": true
-                  },
-                  {
-                    "name": "<:thonkang:219069250692841473>",
-                    "value": "are inline fields",
-                    "inline": true
-                  }
-                ]
-              };
-              message.channel.sendEmbed(embed);
+                "title": "title (VC Mover by ExperienceYT!)",
+                    "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+                    "url": "https://discordapp.com",
+                    "color": 1560522,
+                    "timestamp": "2019-10-06T01:21:31.044Z",
+                    "footer": {
+                      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                      "text": "footer text"
+                    },
+                    "thumbnail": {
+                      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                    },
+                    "image": {
+                      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                    },
+                    "author": {
+                      "name": "author name",
+                      "url": "https://discordapp.com",
+                      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                    },
+                    "fields": [
+                      {
+                        "name": "🤔",
+                        "value": "some of these properties have certain limits..."
+                      },
+                      {
+                        "name": "😱",
+                        "value": "try exceeding some of them!"
+                      },
+                      {
+                        "name": "🙄",
+                        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+                      },
+                      {
+                        "name": "<:thonkang:219069250692841473>",
+                        "value": "these last two",
+                        "inline": true
+                      },
+                      {
+                        "name": "<:thonkang:219069250692841473>",
+                        "value": "are inline fields",
+                        "inline": true
+                      }
+                    ]
+                  };
+                  message.channel.sendEmbed(embed);
             break;    
     }
 });
