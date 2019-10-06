74997a59fc2a3487aec1cbee93853e9a0829233d
anubhavhirani <backspace7676@gmail.com>
2019-10-06 07:57:23 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,7 @@ client.on('message', message => {
         case 'help':
             const embed = {
                 "title": "VC Mover by ExperienceYT!",
-                    "name": " *`Default prefix is set to '.' `* ",
+                 "title": " `Default prefix is set to '.'` ",
                     "description": " `Here are all the commands for your server:` ",
                     "color": 16738560,
                     "timestamp": "2019-10-06T01:21:31.044Z",
