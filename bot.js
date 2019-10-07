250e819cb988fd15057902ef802b753249e75c24
anubhavhirani <backspace7676@gmail.com>
2019-10-07 18:38:04 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,7 +177,7 @@ client.on('message', message => {
         message.channel.send('!tmove 628939897742295063 "Trivia Family!"')
     }}
 
-    if(message.channel.id === "630616948618821664"){return;
+    if(!message.channel.id === "630616948618821664"){ return
 	message.reply('This is an admin command, please use this inside a textchannel named "moveeradmin"');
 						  
     if (args[0].toLowerCase() === 'vclock') {
